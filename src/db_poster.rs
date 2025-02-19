b3e563b58dad41a44398a53c6dc69c605eedffb9
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-18 23:45:31 -0800

    db_poster should have feature parity

--- src/db_poster.rs
+++ src/db_poster.rs
@@ -1,15 +1,10 @@
 use super::types::{Payload, Pkt};
 use crate::entities::*;
 use crate::types::Telem;
+use airqualitymetrics::ActiveModel;
 use anyhow::{Context, Result};
 use chrono::Utc;
 use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseConnection};
-
-enum ActiveModel {
-    Environment(environmentmetrics::ActiveModel),
-    AirQuality(airqualitymetrics::ActiveModel),
-    Device(devicemetrics::ActiveModel),
-}
 
 pub async fn update_metrics(
     db: &DatabaseConnection,
@@ -43,13 +38,14 @@ pub async fn update_metrics(
                             .await
                             .with_context(|| "Failed to insert environment metrics row"))
                             {
-                                Ok(m) => Ok(1),
+                                Ok(_) => Ok(1),
                                 Err(e) => {
                                     error!("{:#}", e);
                                     Ok(0)
                                 }
                             }
                         }
+
                         Telem::AirQuality(data) => {
                             match (airqualitymetrics::ActiveModel {
                                 msg_id: ActiveValue::Set(mp.id),
@@ -73,13 +69,14 @@ pub async fn update_metrics(
                             .await
                             .with_context(|| "Failed to insert air quality metrics row"))
                             {
-                                Ok(m) => Ok(1),
+                                Ok(_) => Ok(1),
                                 Err(e) => {
                                     error!("{:#}", e);
                                     Ok(0)
                                 }
                             }
                         }
+
                         Telem::Device(data) => {
                             match (devicemetrics::ActiveModel {
                                 msg_id: ActiveValue::Set(mp.id),
@@ -89,44 +86,51 @@ pub async fn update_metrics(
                                 voltage: ActiveValue::Set(data.voltage),
                                 channelutil: ActiveValue::Set(data.channel_utilization),
                                 airutil: ActiveValue::Set(data.air_util_tx),
-                                latitude: ActiveValue::NotSet,
+                                latitude: ActiveValue::NotSet, //TODO: investigate default values
                                 longitude: ActiveValue::NotSet,
-                                hwmodel: ActiveValue::NotSet,
                                 longname: ActiveValue::NotSet,
                                 shortname: ActiveValue::NotSet,
+                                hwmodel: ActiveValue::NotSet,
                             }
                             .insert(db)
                             .await
                             .with_context(|| "Failed to insert device metrics row"))
                             {
-                                Ok(m) => Ok(1),
+                                Ok(_) => Ok(1),
                                 Err(e) => {
                                     error!("{:#}", e);
                                     Ok(0)
                                 }
                             }
                         }
+
                         Telem::Power(_data) => {
                             // Not sure what we want to do with these metrics
                             Ok(0)
                         }
                     },
+
                     Payload::NodeinfoApp(data) => {
                         // Only updates user information
                         match (nodeinfo::ActiveModel {
                             node_id: ActiveValue::Set(mp.from),
+                            longname: ActiveValue::Set(data.long_name),
+                            shortname: ActiveValue::Set(data.short_name),
+                            hwmodel: ActiveValue::Set(data.hw_model),
+                            deployment_location: ActiveValue::Set(dep_loc.to_string()),
                         }
                         .insert(db)
                         .await
                         .with_context(|| "Failed to insert node info row"))
                         {
-                            Ok(m) => Ok(1),
+                            Ok(_) => Ok(1),
                             Err(e) => {
                                 error!("{:#}", e);
                                 Ok(0)
                             }
                         }
                     }
+
                     Payload::PositionApp(data) => {
                         // Updates the position for a given node id that is included in the
                         // packet sent from the mesh
@@ -134,18 +138,28 @@ pub async fn update_metrics(
                             msg_id: ActiveValue::Set(mp.id),
                             node_id: ActiveValue::Set(mp.from),
                             time: ActiveValue::Set(Utc::now().naive_utc()),
+                            latitude: ActiveValue::Set(data.latitude_i),
+                            longitude: ActiveValue::Set(data.longitude_i),
+                            battery_levels: ActiveValue::NotSet, //TODO: investigate default values
+                            voltage: ActiveValue::NotSet,
+                            channelutil: ActiveValue::NotSet,
+                            airutil: ActiveValue::NotSet,
+                            longname: ActiveValue::NotSet,
+                            shortname: ActiveValue::NotSet,
+                            hwmodel: ActiveValue::NotSet,
                         }
                         .insert(db)
                         .await
                         .with_context(|| "Failed to insert device metrics row"))
                         {
-                            Ok(m) => Ok(1),
+                            Ok(_) => Ok(1),
                             Err(e) => {
                                 error!("{:#}", e);
                                 Ok(0)
                             }
                         }
                     }
+
                     _ => {
                         // Other payloads are unhandled, but there are some that may be of
                         // interest to us. Namely: TextMessageApp, RemoteHardwareApp (if we
@@ -174,24 +188,56 @@ pub async fn update_metrics(
                 Ok(0)
             }
         }
+
         Pkt::NInfo(ni) => {
+            let mut rv = 0;
             // This is the NodeInfo that is communicated directly over serial to our process
             // It does not have a MeshPkt (Meshtastic Packet from LoRa)
-            // So we will need to decide what to do with its User, Position, and DeviceMetrics data
-            // We could provide a dummy packet, or we could just update the 'static' table to
-            // reflect the node db on the device
-            match (nodeinfo::ActiveModel {}
+            let dm = devicemetrics::ActiveModel {
+                msg_id: ActiveValue::Set(fake_msg_id.expect("Fake_msg_id not provided")),
+                node_id: ActiveValue::Set(ni.num),
+                time: ActiveValue::Set(Utc::now().naive_utc()),
+                latitude: ActiveValue::Set(ni.position.as_ref().and_then(|pos| pos.latitude_i)),
+                longitude: ActiveValue::Set(ni.position.as_ref().and_then(|pos| pos.longitude_i)),
+                battery_levels: ActiveValue::Set(
+                    ni.device_metrics.as_ref().and_then(|dm| dm.battery_level),
+                ),
+                voltage: ActiveValue::Set(ni.device_metrics.as_ref().and_then(|dm| dm.voltage)),
+                channelutil: ActiveValue::Set(
+                    ni.device_metrics
+                        .as_ref()
+                        .and_then(|dm| dm.channel_utilization),
+                ),
+                airutil: ActiveValue::Set(ni.device_metrics.as_ref().and_then(|dm| dm.air_util_tx)),
+                longname: ActiveValue::Set(ni.user.as_ref().map(|u| u.long_name.clone())),
+                shortname: ActiveValue::Set(ni.user.as_ref().map(|u| u.short_name.clone())),
+                hwmodel: ActiveValue::Set(ni.user.as_ref().map(|u| u.hw_model)),
+            }
+            .insert(db)
+            .await
+            .with_context(|| "Failed to insert device metrics row");
+            if dm.is_ok() {
+                rv += 1;
+            }
+            // Now update the nodeinfo table if needed
+            if let Some(user) = ni.user {
+                let nm = nodeinfo::ActiveModel {
+                    node_id: ActiveValue::Set(ni.num),
+                    longname: ActiveValue::Set(user.long_name),
+                    shortname: ActiveValue::Set(user.short_name),
+                    hwmodel: ActiveValue::Set(user.hw_model),
+                    deployment_location: ActiveValue::Set(dep_loc.to_string()),
+                }
                 .insert(db)
                 .await
-                .with_context(|| "Failed to insert environment metrics row"))
-            {
-                Ok(m) => Ok(1),
-                Err(e) => {
-                    error!("{:#}", e);
-                    Ok(0)
+                .with_context(|| "Failed to insert device metrics row");
+                if nm.is_ok() {
+                    rv += 1;
                 }
             }
+            Ok(rv)
         }
+
         _ => {
             // Only other type implemented at this time is MyNodeInfo, which just provides our
             // node's ID number which could be used for the managing of local state if needed,
