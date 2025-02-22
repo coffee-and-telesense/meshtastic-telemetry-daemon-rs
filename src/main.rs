37eadd7bf049dc4ed9f0e4e81f0b9f46f9f77a4f
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-21 17:51:43 -0800

    Move sea-orm macros to src/dto/entities, add docs to some

--- src/main.rs
+++ src/main.rs
@@ -1,232 +1,38 @@
 #![warn(missing_docs)]
 #![warn(clippy::cargo)]
 #![warn(clippy::pedantic)]
+// Unfortunately we have duplicate dependencies with different versions
+#![allow(clippy::multiple_crate_versions)]
 
+use crate::db::connection::update_metrics;
+use crate::dto::packet_handler;
+use crate::util::{
+    config::{build_db_connection_string, get_cfg_string, get_serial_port, read_config},
+    log::set_logger,
+    types::{GatewayState, Pkt},
+};
+use anyhow::{Context, Result};
 #[cfg(feature = "debug")]
 use log::{error, info, warn};
-#[cfg(debug_assertions)]
-use std::collections::HashMap;
-use std::{
-    io::{self, BufRead},
-    sync::{Arc, Mutex},
-};
-#[cfg(not(debug_assertions))]
-extern crate syslog;
-#[cfg(not(debug_assertions))]
-#[macro_use]
-extern crate log;
-use anyhow::{Context, Result};
-use config::Config;
-use db_poster::update_metrics;
-#[cfg(not(debug_assertions))]
-use log::LevelFilter;
 use meshtastic::api::StreamApi;
 use meshtastic::utils;
 use sea_orm::{ConnectOptions, Database};
-use serde::Deserialize;
 #[cfg(feature = "print-packets")]
 use serde_json::to_string_pretty;
-#[cfg(not(debug_assertions))]
-use syslog::{BasicLogger, Facility, Formatter3164};
+use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc;
-use types::GatewayState;
 
 /// Database interaction module
-pub mod db_poster;
-/// Entities Module for database schema
-pub mod entities;
-/// Handle packets module
-pub mod packet_handler;
-/// Types module
-pub mod types;
+pub(crate) mod db;
+/// Handle data transfer objects
+pub(crate) mod dto;
 /// Utilities module
-pub mod utilities;
+pub(crate) mod util;
 
-///
-///
-/// # Arguments
-/// *
+/// Main
 ///
 /// # Returns
-/// *
-fn read_config(p: &str) -> config::Config {
-    match config::Config::builder()
-        .add_source(config::File::with_name(p))
-        .build()
-        .with_context(|| format!("Failed to read config file from {p}"))
-    {
-        Ok(rv) => {
-            #[cfg(debug_assertions)]
-            println!(
-                "{:?}",
-                rv.clone()
-                    .try_deserialize::<HashMap<String, String>>()
-                    .expect("Failed to deserialize config values in println of read_config()")
-            );
-            rv
-        }
-        Err(e) => {
-            panic!("{e:#}");
-        }
-    }
-}
-
-///
-///
-/// # Arguments
-/// *
-///
-/// # Returns
-/// *
-fn get_cfg<'d, T: Deserialize<'d>>(cfg: &Config, key: &str) -> T {
-    match cfg
-        .get::<T>(key)
-        .with_context(|| format!("Failed to read {key} from config"))
-    {
-        Ok(rv) => rv,
-        Err(e) => {
-            panic!("{e:#}");
-        }
-    }
-}
-
-///
-///
-/// # Arguments
-/// *
-///
-/// # Returns
-/// *
-fn get_cfg_string(cfg: &Config, key: &str) -> String {
-    match cfg
-        .get_string(key)
-        .with_context(|| format!("Failed to read {key} from config"))
-    {
-        Ok(rv) => rv,
-        Err(e) => {
-            panic!("{e:#}");
-        }
-    }
-}
-
-///
-///
-/// # Arguments
-/// *
-///
-/// # Returns
-/// *
-fn db_connection(cfg: &Config) -> String {
-    // Parse config with error handling
-    format!(
-        "postgres://{}:{}@{}:{}/{}",
-        get_cfg_string(cfg, "user"),
-        get_cfg_string(cfg, "password"),
-        get_cfg_string(cfg, "host"),
-        get_cfg::<u16>(cfg, "port"),
-        get_cfg_string(cfg, "dbname")
-    )
-}
-
-///
-///
-/// # Arguments
-/// *
-///
-/// # Returns
-/// *
-fn get_serial_port(cfg: &Config) -> String {
-    match cfg
-        .get_string("serial_port")
-        .with_context(|| "Failed to read serial_port from config file")
-    {
-        Err(e) => {
-            #[cfg(debug_assertions)]
-            {
-                eprintln!("{e:#}");
-                eprintln!("Prompting user for serial port instead");
-            }
-            #[cfg(not(debug_assertions))]
-            {
-                error!("{e:#}");
-                warn!("Prompting user for serial port instead");
-            }
-            match utils::stream::available_serial_ports()
-                .with_context(|| "Failed to enumerate list of serial ports")
-            {
-                Ok(ap) => println!("Available ports: {ap:?}"),
-                Err(e) => {
-                    error!("{e:#}");
-                    warn!("User will input their own serial port");
-                }
-            }
-            println!("Enter the name of a port to connect to:");
-
-            let stdin = io::stdin();
-            match stdin
-                .lock()
-                .lines()
-                .next()
-                .expect("Failed to find next line")
-                .with_context(|| "Could not read from stdin")
-            {
-                Ok(sp) => sp,
-                Err(e) => {
-                    eprintln!("No serial port provided by user");
-                    panic!("{e:#}");
-                }
-            }
-        }
-        Ok(sp) => sp,
-    }
-}
-
-///
-///
-/// # Arguments
-/// *
-///
-/// # Returns
-/// *
-fn set_logger() {
-    #[cfg(feature = "debug")]
-    {
-        colog::init();
-    }
-    #[cfg(not(debug_assertions))]
-    {
-        let formatter = Formatter3164 {
-            facility: Facility::LOG_USER, //TODO: this could probably be something else, check libc
-            hostname: None,
-            process: "mesh_telem".into(),
-            pid: 0,
-        };
-        match syslog::unix(formatter).with_context(|| "Could not connect to syslog posix socket") {
-            Ok(logger) => {
-                //TODO: should be warn on actual release instead of Info
-                let _ = log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
-                    .map(|()| log::set_max_level(LevelFilter::Info))
-                    .with_context(|| "Failed to set logger to syslog")
-                    .inspect_err(|e| {
-                        error!("{e:#}");
-                        warn!("Continuing execution");
-                    });
-            }
-            Err(e) => {
-                error!("{e:#}");
-                warn!("Continuing execution");
-            }
-        }
-    }
-}
-
-///
-///
-/// # Arguments
-/// *
-///
-/// # Returns
-/// *
+/// * Result of () or an Error
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(debug_assertions)]
@@ -240,7 +46,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let state = Arc::new(Mutex::new(GatewayState::new()));
 
     // Connect to postgres db
-    let mut opt = ConnectOptions::new(db_connection(&settings));
+    let mut opt = ConnectOptions::new(build_db_connection_string(&settings));
     opt.sqlx_logging(true)
         .sqlx_logging_level(log::LevelFilter::Debug);
     let db = Database::connect(opt)
@@ -277,7 +83,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         });
         if let Some(pkt) = rx.recv().await.unwrap() {
             match pkt.clone() {
-                types::Pkt::Mesh(mp) => {
+                Pkt::Mesh(mp) => {
                     #[cfg(feature = "print-packets")]
                     println!("{}", to_string_pretty(&mp).unwrap());
                     let res = update_metrics(&db, pkt, None, &deployment_loc)
@@ -288,7 +94,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         Err(e) => error!("{e:#}"),
                     }
                 }
-                types::Pkt::NInfo(ni) => {
+                Pkt::NInfo(ni) => {
                     #[cfg(feature = "print-packets")]
                     println!("{}", to_string_pretty(&ni).unwrap());
                     let fake = state
@@ -312,7 +118,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         }
                     }
                 }
-                types::Pkt::MyNodeInfo(mi) => {
+                Pkt::MyNodeInfo(mi) => {
                     #[cfg(feature = "print-packets")]
                     println!("{}", to_string_pretty(&mi).unwrap());
                 }
