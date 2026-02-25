c3215c1fdb57f0893999f77ec72e2f772b4eee83
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2026-02-25 14:45:07 -0800

    chore(models): remove models dead code

--- src/dto/models/nodeinfo.rs
+++
@@ -1,8 +0,0 @@
-#[derive(derive_new::new, sqlx::FromRow)]
-pub struct Nodeinfo {
-    node_id: sqlx::postgres::types::Oid,
-    longname: String,
-    shortname: String,
-    hwmodel: i32,
-    deployment_location: String,
-}
