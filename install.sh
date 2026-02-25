6714ce801ce4fb7abde9811e7def2c97bdb1eb45
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2026-02-24 17:48:56 -0800

    chore(install script): update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 # Install script for RPi4s to setup the mesh telemetry user service for 915 and 433 deployments
+mv mesh-telem433.service /etc/systemd/system/mesh-telem433.service
+mv mesh-telem915.service /etc/systemd/system/mesh-telem915.service
+mv 92-usb-input-no-powersave.rules /etc/udev/rules.d/92-usb-input-no-powersave.rules
+
+# Reload systemd services
+systemctl daemon-reload
+
+# Reload udev rules
+udevadm control --reload-rules && udevadm trigger
